7812e4ea045f58874fe0f01e8dea304c80fb6ebb
kasia-kusz <katarzyna.kuszx@gmail.com>
2019-03-23 21:26:26 +0100

    Okno logowania - dopasowanie nazw

--- src/Okna/Dziennik.java
+++ src/Okna/Dziennik.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dziennik;
+package Okna;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
--- src/Okna/LogowanieController.java
+++ src/Okna/LogowanieController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dziennik;
+package Okna;
 
 import java.io.IOException;
 import java.net.URL;
