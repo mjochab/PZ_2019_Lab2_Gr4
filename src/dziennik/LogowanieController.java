8edac7fc4d9b38e5de4be8825d9c5481c4ade8a2
kasia-kusz <katarzyna.kuszx@gmail.com>
2019-03-23 20:43:40 +0100

    Okno logowania - bez funkcjonalności

---
+++ src/dziennik/LogowanieController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dziennik;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+/**
+ * FXML Controller class
+ *
+ * @author Kasia
+ */
+public class LogowanieController implements Initializable {
+    @FXML
+    private Text login;
+    @FXML
+    private Text haslo;
+    @FXML
+    private Button zalogujbtn;
+    @FXML
+    private AnchorPane rootPane;
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    @FXML
+    private void handleButtonAction(ActionEvent event) throws IOException {
+
+    }
+       
+    
+}
