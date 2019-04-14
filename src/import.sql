b66d33877f1803a0315392343b237aeacd0afd43
drozd1krystian <37024620+drozd1krystian@users.noreply.github.com>
2019-04-14 16:53:59 +0200

    Hibernate Jary

---
+++ src/import.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO uczen (pesel,nazwa_klasy,imie,nazwisko) values (123456,'3B','Wojtus','Karakan')
+INSERT INTO uczen (pesel,nazwa_klasy,imie,nazwisko) values (123457,'3B','Wojtus','Karakan')
+INSERT INTO uczen (pesel,nazwa_klasy,imie,nazwisko) values (123458,'3B','Wojtus','Karakan')
+INSERT INTO uczen (pesel,nazwa_klasy,imie,nazwisko) values (123459,'3B','Wojtus','Karakan')
