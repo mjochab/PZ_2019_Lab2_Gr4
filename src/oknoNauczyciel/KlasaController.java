89ce900aed11311980d1631091cba7f8b16948f7
Vethavear <adrian.bury@gmail.com>
2019-04-21 23:50:15 +0200

    Nauczyciel - próba przekazywania klasy i nazwy zalogowanego nauczyciela okienko dalej.

--- src/oknoNauczyciel/KlasaController.java
+++ src/oknoNauczyciel/KlasaController.java
@@ -13,11 +13,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
-
-
 
 public class KlasaController implements Initializable {
 
@@ -33,14 +32,18 @@ public class KlasaController implements Initializable {
     private AnchorPane rootPane;
     @FXML
     private Button powrotbtn;
-    
+    @FXML
+    private Label userid;
+    private String klasa = null;
+    private String username = null;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        // dziala niestety po inicjalizacji, wiec nie ustawi username :(
+         wstawUseraDoZalogowanoJako(username);
     }
 
     @FXML
@@ -50,15 +53,18 @@ public class KlasaController implements Initializable {
 
     @FXML
     private void logout(ActionEvent event) throws IOException {
-            AnchorPane pane = FXMLLoader.load(getClass().getResource("/Okna/Logowanie.fxml"));
-             rootPane.getChildren().setAll(pane);
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("/Okna/Logowanie.fxml"));
+        rootPane.getChildren().setAll(pane);
+
     }
 
     //ładujemy okno z ocenami uczenia.
     @FXML
     private void LoadPowrot(ActionEvent event) throws IOException {
+
         AnchorPane pane = FXMLLoader.load(getClass().getResource("NauczycielKlasy.fxml"));
         rootPane.getChildren().setAll(pane);
+        System.out.println(username);
     }
 
     @FXML
@@ -72,12 +78,27 @@ public class KlasaController implements Initializable {
         AnchorPane pane = FXMLLoader.load(getClass().getResource("DodajNieobecnosc.fxml"));
         rootPane.getChildren().setAll(pane);
     }
+
     @FXML
     private void LoadUsprawiedliw(ActionEvent event) throws IOException {
         AnchorPane pane = FXMLLoader.load(getClass().getResource("DodajUsprawiedliwienie.fxml"));
         rootPane.getChildren().setAll(pane);
     }
-    
 
-    
+    private void wstawUseraDoZalogowanoJako(String username) {
+
+        userid.setText(username);
+
+    }
+
+    public void przekazKlaseIusername(String klasa, String username) {
+        this.username = username;
+        this.klasa = klasa;
+
+    }
+
+    public String getKlasa() {
+        return klasa;
+    }
+
 }
--- src/oknoNauczyciel/NauczycielKlasyController.java
+++ src/oknoNauczyciel/NauczycielKlasyController.java
@@ -10,35 +10,40 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import utilities.HibernateUtil;
-
-
 
 public class NauczycielKlasyController implements Initializable {
 
-    // do zrobienia dynamicznie
-    //Benton Cos tam
-    static final Long pesel=22222222220L;
-    
+    //W oczekiwaniu na przekazanie wartosci z logowania, tymczasowe stałe:
+    private Long pesel = 22222222220L;
+    private String username = "Wojtus";
+
     @FXML
     private Button klasa1;
     @FXML
     private Button klasa2;
     @FXML
     private Button wylogujbtn;
-
-    @FXML 
+    @FXML
+    private Label userid;
+    @FXML
     private Pane panelButtonow;
     @FXML
     private AnchorPane rootPane;
@@ -49,6 +54,7 @@ public class NauczycielKlasyController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         wybierzKlaseButtony();
+        wstawUseraDoZalogowanoJako(username);
         // TODO
     }
 
@@ -59,49 +65,60 @@ public class NauczycielKlasyController implements Initializable {
 
     @FXML
     private void logout(ActionEvent event) throws IOException {
-            AnchorPane pane = FXMLLoader.load(getClass().getResource("/Okna/Logowanie.fxml"));
-             rootPane.getChildren().setAll(pane);
-    }
-
-    //ładujemy okno z ocenami uczenia.
-    @FXML
-    private void LoadKlasa(ActionEvent event) throws IOException {
-        AnchorPane pane = FXMLLoader.load(getClass().getResource("Klasa.fxml"));
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("/Okna/Logowanie.fxml"));
         rootPane.getChildren().setAll(pane);
     }
-    // do skonczenia
 
+    private void wybierzKlaseButtony() {
 
-    
-    
-    
-    
-    private void wybierzKlaseButtony(){
-      
-      
-      String [] nazwyKlas = utilities.HibernateUtil.zwrocKlasyKtoreUcze(pesel);   
-      Button[] listaButtonow = new Button[nazwyKlas.length];
+        String[] nazwyKlas = utilities.HibernateUtil.zwrocKlasyKtoreUcze(pesel);
+        Button[] listaButtonow = new Button[nazwyKlas.length];
         for (int i = 0; i < nazwyKlas.length; i++) {
-          Button b = new Button(nazwyKlas[i]);
-          b.setId("klasax"+i);
-          b.setText(nazwyKlas[i]);
-          b.addEventHandler(MouseEvent.MOUSE_CLICKED, 
-                (event -> b.setText("kulke"))); 
-          listaButtonow[i]=b;
+            Button b = new Button(nazwyKlas[i]);
+            b.setId("klasax" + i);
+            b.setText(nazwyKlas[i]);
+            b.addEventHandler(MouseEvent.MOUSE_CLICKED,
+                    (event -> {
+                        try {
+                            zaladujKlase(b);
+                        } catch (IOException ex) {
+                            Logger.getLogger(NauczycielKlasyController.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }));
+            listaButtonow[i] = b;
         }
-        
+
         panelButtonow.getChildren().clear();
         panelButtonow.getChildren().addAll(listaButtonow);
+
+    }
+
+    public void przekazNazweUzytkownika(String username) {
+
+        this.username = username;
+
+    }
+
+    private void wstawUseraDoZalogowanoJako(String username) {
+
+        userid.setText(username);
+
+    }
+
+    private void zaladujKlase(Button b) throws IOException {
+
+        Stage st = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Klasa.fxml"));
+        Region root = (Region) fxmlLoader.load();
+
+        Scene scene = new Scene(root);
+        st.setScene(scene);
+
+        KlasaController mainController = fxmlLoader.<KlasaController>getController();
+        mainController.przekazKlaseIusername(b.getText(),username);
         
-    
-}
-    
-    
-    
-    
-    
-    private void wstawNazweUzytkownika(){
-        
-        
+
+        st.show();
+
     }
 }
