35d581f88ce2a4f7aa5975d1445b4a755fc3091d
Vethavear <adrian.bury@gmail.com>
2019-04-24 21:57:03 +0200

    Nauczyciel - merge

--- src/utilities/HibernateTest.java
+++ src/utilities/HibernateTest.java
@@ -13,6 +13,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
@@ -89,6 +90,13 @@ public class HibernateTest {
         
         return klasy;
     }
+        
+        public static void testyQuery(){
+          Query query = entityManager.createQuery(
+              "SELECT e FROM Employee e where e.salary < (SELECT AVG(e2.salary) FROM Employee e2)");
+   List<Uczen> resultList = query.getResultList();
+            
+        }
             
     public static void main(String[] args) throws ParseException {
 
